4fd4f5dfdd524b3eb5407b4f0c7e483d8f296ed5
invine <invine.invine@yandex.ru>
2019-06-17 12:46:31 +0300

    updated week8

--- week10/main.go
+++ week10/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 	x := 0
+	var wg sync.WaitGroup
+	wg.Add(2)
 	go func() {
 		//time.Sleep(time.Second)
 		fmt.Println(x)
+		wg.Done()
 	}()
-	go func() { x++ }()
+	go func() {
+		x++
+		wg.Done()
+	}()
 	// time.Sleep(2 * time.Second)
+	wg.Wait()
 	fmt.Println("done")
 }
