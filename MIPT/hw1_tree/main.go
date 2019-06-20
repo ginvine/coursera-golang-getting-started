3bad7f4e050b114a8d34b43310c6bf5b00df1019
invine <invine.invine@yandex.ru>
2019-06-20 10:20:52 +0300

    implemented tree

--- MIPT/hw1_tree/main.go
+++ MIPT/hw1_tree/main.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
+	"sort"
 )
 
 func main() {
@@ -19,4 +19,58 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	return dirTreeHelper("", out, path, printFiles)
+}
+
+func dirTreeHelper(prefix string, out io.Writer, path string, printFiles bool) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	fileInfos, err := file.Readdir(0)
+
+	sort.Slice(fileInfos, func(i, j int) bool { return fileInfos[i].Name() < fileInfos[j].Name() })
+
+	if !printFiles {
+		fileInfos = removeFiles(fileInfos)
+	}
+
+	for i, f := range fileInfos {
+		p := "%s├───%s\n"
+		if i == len(fileInfos)-1 {
+			p = "%s└───%s\n"
+		}
+
+		s := f.Name()
+		if !f.IsDir() {
+			s = fmt.Sprintf("%s (%db)", s, f.Size())
+			if f.Size() == 0 {
+				s = fmt.Sprintf("%s (empty)", f.Name())
+			}
+		}
+
+		fmt.Fprintf(out, p, prefix, s)
+
+		if f.IsDir() {
+			pr := prefix + "│\t"
+			if i == len(fileInfos)-1 {
+				pr = prefix + "\t"
+			}
+			dirTreeHelper(pr, out, filepath.Join(path, f.Name()), printFiles)
+		}
+	}
+	return nil
+}
+
+func removeFiles(fi []os.FileInfo) []os.FileInfo {
+	res := []os.FileInfo{}
+	for _, f := range fi {
+		if f.IsDir() {
+			res = append(res, f)
+		}
+	}
+	return res
 }
