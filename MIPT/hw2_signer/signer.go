93f8a93ce15dbfa7e58c39347510adc1c090e4a5
invine <invine.invine@yandex.ru>
2019-06-21 12:31:57 +0300

    week2

---
+++ MIPT/hw2_signer/signer.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type job func(in, out chan interface{})
+
+func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{})
+	out := make(chan interface{})
+	for _, j := range jobs {
+		go j(in, out)
+		in = out
+		out = make(chan interface{})
+	}
+}
+
+func main() {
+	jobs := []job{
+		func(in, out chan interface{}) {
+			out <- 1
+		},
+		func(in, out chan interface{}) {
+			data := <-in
+			fmt.Println(data)
+		},
+	}
+	ExecutePipeline(jobs...)
+	fmt.Scanln()
+}
+
+func SingleHash(in, out chan interface{}) {
+	for input := range in {
+		data := input.(string)
+		out <- DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+	}
+
+}
+
+func MultiHash(in, out chan interface{}) {
+	for input := range in {
+		data := input.(string)
+		result := ""
+		for i := 0; i < 6; i++ {
+			result = result + DataSignerCrc32(strconv.Itoa(i)+data)
+		}
+	}
+}
+
+func CombineResults(in, out chan interface{}) {
+	result := []string{}
+	for input := range in {
+		data := input.(string)
+		result = append(result, data)
+	}
+	sort.Strings(result)
+	out <- strings.Join(result, "_")
+}
